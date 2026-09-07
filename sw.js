b83bd80f482a22854640a5c1d044d8c1be8d4f96
oa2572000-svg <oa2572000@gmail.com>
2026-09-08 02:07:46 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-const C='omar-academy-v1';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
+const C='omar-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
